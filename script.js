001790cf5588b3f08db2496d1aae1b61530eaf3f
KayJay-26 <khrithikjupalli@gmail.com>
2026-06-08 14:45:33 +0530

    Update index.html and add script.js for movie search functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+const movieInput = document.getElementById("movieInput");
+const searchBtn = document.getElementById("searchBtn");
+const movieResult = document.getElementById("movieResult");
+
+searchBtn.addEventListener("click", async () => {
+  const response = await fetch("https://ghibliapi.vercel.app/films");
+
+  const data = await response.json();
+
+  const movie = data.find((item) =>
+    item.title.toLowerCase().includes(movieName.toLowerCase()),
+  );
+});
